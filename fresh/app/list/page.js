e39349a7455bbc50c1964ff27b86faea15733da3
InKyuHwang001 <hwanginkyu42@naver.com>
2025-01-25 09:27:33 +0900

    useSate

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,6 +1,9 @@
+'use client';
+import {useState} from "react";
 
 export default function List() {
     let products = ['Tomatoes', 'Pasta', 'Coconut']
+    let [cnt, setCnt] = useState(0);
     return (
         <div>
             <h4 className="title">상품목록</h4>
@@ -10,9 +13,11 @@ export default function List() {
                         <div className="food" key={idx}>
                             <img src={`/food${idx}.png`} className="food-img"/>
                             <h3>{product} ${idx}</h3>
+                            <span> {cnt} </span>
+                            <button onClick={()=>{setCnt(cnt+1)}}>+</button>
                         </div>
                     )
-                }) }
+                })}
         </div>
     )
 }
