a60d80a3526ac380657694dd48afb1516f7d3a48
InKyuHwang001 <hwanginkyu42@naver.com>
2025-01-30 20:24:52 +0900

    error, loading, notFound

--- memo/app/detail/[id]/page.js
+++ memo/app/detail/[id]/page.js
@@ -1,9 +1,14 @@
 import { ObjectId } from "mongodb";
 import { connectDB } from "@/util/database.js"
+import {notFound} from "next/navigation";
 
 export default async function Detail(props) {
     let db = (await connectDB).db('forum')
     let result = await db.collection('post').findOne({_id : new ObjectId(props.params._id)});
+
+    if (result === null) {
+        return notFound()
+    }
 
     return (
         <div>
