230944755b1ee6387590943a55ce9252742941c3
InKyuHwang001 <hwanginkyu42@naver.com>
2025-01-30 19:34:23 +0900

    edit

--- memo/app/layout.js
+++ memo/app/layout.js
@@ -1,12 +1,17 @@
 import "./globals.css";
+import Link from "next/link";
 
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="kr">
-      <body>
+export default function RootLayout({children}) {
+    return (
+        <html lang="kr">
+        <body>
+        <div className="navbar">
+            <Link href="/" className="logo">HwangMemo</Link>
+            <Link href="/list">List</Link>
+        </div>
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
--- memo/app/list/page.js
+++ memo/app/list/page.js
@@ -1,5 +1,6 @@
 import {connectDB} from "@/util/database";
 import Link from "next/link";
+import DetailLink from "@/app/list/DetailLink";
 
 export default async function Page() {
     let client = await connectDB;
@@ -12,6 +13,9 @@ export default async function Page() {
                 result.map((a, i) =>
                     <div className="list-item">
                         <Link href={'/detail/' + result[i]._id}><h4>{a.title}</h4></Link>
+                        <DetailLink/>
+                        <Link href={'/edit/' + result[i]._id} className="list-btn">✏️</Link>
+
                         <p>1월 1일</p>
                     </div>
                 )
