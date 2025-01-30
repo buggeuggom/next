f0601d80e833e4b202df77df4c45e49a3470ec82
InKyuHwang001 <hwanginkyu42@naver.com>
2025-01-30 20:16:29 +0900

    useEffect

--- memo/app/list/page.js
+++ memo/app/list/page.js
@@ -1,6 +1,8 @@
 import {connectDB} from "@/util/database";
 import ListItem from "@/app/list/ListItem";
 
+export const dynamic = 'force-dynamic' //'force-static'
+
 export default async function Page() {
     let client = await connectDB;
     const db = client.db('forum');
